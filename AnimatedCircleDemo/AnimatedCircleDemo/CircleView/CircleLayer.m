134a02d63dc3b20055e43c61dfb6d68d75c5a399
KittenYang <kittenyang@icloud.com>
2015-07-24 00:54:20 +0800

    dashed line & continuous line.

--- AnimatedCircleDemo/AnimatedCircleDemo/CircleView/CircleLayer.m
+++ AnimatedCircleDemo/AnimatedCircleDemo/CircleView/CircleLayer.m
@@ -37,18 +37,30 @@
     //外接矩形的中心坐标
 //    CGPoint rectCenter = CGPointMake(self.outsideRect.origin.x + self.outsideRect.size.width/2 , self.outsideRect.origin.y + self.outsideRect.size.height/2);
     
-    
 //    CGPoint pointA = CGPointMake(rectCenter.x ,self.currentRect.origin.y + extra);
 //    CGPoint pointB = CGPointMake(self.scrollDirection == ScrollDirectionLeft ? rectCenter.x + self.currentRect.size.width/2 : rectCenter.x + self.currentRect.size.width/2 + extra*2 ,rectCenter.y);
 //    CGPoint pointC = CGPointMake(rectCenter.x ,rectCenter.y + self.currentRect.size.height/2 - extra);
 //    CGPoint pointD = CGPointMake(self.scrollDirection == ScrollDirectionLeft ? self.currentRect.origin.x - extra*2 : self.currentRect.origin.x, rectCenter.y);
     
-    UIBezierPath *ovalPath = [UIBezierPath bezierPathWithRect:self.outsideRect];
+    //外接虚线矩形
+    UIBezierPath *rectPath = [UIBezierPath bezierPathWithRect:self.outsideRect];
+    CGContextAddPath(ctx, rectPath.CGPath);
+    CGContextSetStrokeColorWithColor(ctx, [UIColor blackColor].CGColor);
+    CGContextSetLineWidth(ctx, 1.0);
+    CGFloat dash[] = {5.0, 5.0};
+    CGContextSetLineDash(ctx, 0.0, dash, 2); //1
+    CGContextStrokePath(ctx); //给线条填充颜色
+    
+    
+    //圆的边界
+    UIBezierPath *ovalPath = [UIBezierPath bezierPathWithOvalInRect:self.outsideRect];
     CGContextAddPath(ctx, ovalPath.CGPath);
     CGContextSetStrokeColorWithColor(ctx, [UIColor blackColor].CGColor);
     CGContextSetFillColorWithColor(ctx, [UIColor redColor].CGColor);
+    CGContextSetLineDash(ctx, 0, NULL, 0); //2
+    CGContextDrawPath(ctx, kCGPathFillStroke); //同时给线条和线条包围的内部区域填充颜色
     
-    CGContextDrawPath(ctx, kCGPathFillStroke); // 同时填充颜色和边界
+    //ctx字面意思是上下文，你可以理解为一块全局的画布。也就是说，一旦在某个地方改了画布的一些属性，其他任何使用画布的属性的时候都是改了之后的。比如上面在 //1 中把线条样式改成了虚线，那么在下文 //2 中如果不恢复成连续的直线，那么画出来的依然是//1中的虚线样式。
 
 }
 
