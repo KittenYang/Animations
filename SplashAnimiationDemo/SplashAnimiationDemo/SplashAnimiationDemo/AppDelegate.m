e3a21bb09a75310136bd2980c7e457600f2f44aa
KittenYang <kittenyang@icloud.com>
2015-08-09 20:55:14 +0800

    splash animation done

--- SplashAnimiationDemo/SplashAnimiationDemo/SplashAnimiationDemo/AppDelegate.m
+++ SplashAnimiationDemo/SplashAnimiationDemo/SplashAnimiationDemo/AppDelegate.m
@@ -17,8 +17,78 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+
+    [self.window makeKeyAndVisible];
+    self.window.backgroundColor = [UIColor colorWithRed:128.0/255.0 green:0.0/255.0 blue:0.0/255.0 alpha:1.0];
+    
+    UINavigationController *navc = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateInitialViewController];
+    self.window.rootViewController = navc;
+    
+    
+    //logo mask
+    CALayer *maskLayer = [CALayer layer];
+    maskLayer.contents = (id)[UIImage imageNamed:@"logo"].CGImage;
+    maskLayer.position = navc.view.center;
+    maskLayer.bounds = CGRectMake(0, 0, 60, 60);
+    navc.view.layer.mask = maskLayer;
+    
+    //logo mask background view
+    UIView *maskBackgroundView = [[UIView alloc]initWithFrame:navc.view.bounds];
+    maskBackgroundView.backgroundColor = [UIColor whiteColor];
+    [navc.view addSubview:maskBackgroundView];
+    [navc.view bringSubviewToFront:maskBackgroundView];
+    
+    
+    //logo mask animation
+    CAKeyframeAnimation *logoMaskAnimaiton = [CAKeyframeAnimation animationWithKeyPath:@"bounds"];
+    logoMaskAnimaiton.duration = 1.0f;
+    logoMaskAnimaiton.beginTime = CACurrentMediaTime() + 1.0f;//延迟一秒
+
+    CGRect initalBounds = maskLayer.bounds;
+    CGRect secondBounds = CGRectMake(0, 0, 50, 50);
+    CGRect finalBounds  = CGRectMake(0, 0, 2000, 2000);
+    logoMaskAnimaiton.values = @[[NSValue valueWithCGRect:initalBounds],[NSValue valueWithCGRect:secondBounds],[NSValue valueWithCGRect:finalBounds]];
+    logoMaskAnimaiton.keyTimes = @[@(0),@(0.5),@(1)];
+    logoMaskAnimaiton.timingFunctions = @[[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut],[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut],[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut]];
+    logoMaskAnimaiton.removedOnCompletion = NO;
+    logoMaskAnimaiton.fillMode = kCAFillModeForwards;
+    [navc.view.layer.mask addAnimation:logoMaskAnimaiton forKey:@"logoMaskAnimaiton"];
+    
+    
+    //maskBackgroundView fade animation
+    [UIView animateWithDuration:0.1 delay:1.35 options:UIViewAnimationOptionCurveEaseIn animations:^{
+
+        maskBackgroundView.alpha = 0.0;
+        
+    } completion:^(BOOL finished) {
+        
+        [maskBackgroundView removeFromSuperview];
+        
+    }];
+    
+    
+    //navc.view bounce animation
+    [UIView animateWithDuration:0.25 delay:1.3 options:UIViewAnimationOptionTransitionNone animations:^{
+
+        navc.view.transform = CGAffineTransformMakeScale(1.05, 1.05);
+        
+    } completion:^(BOOL finished) {
+
+        [UIView animateWithDuration:0.3 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+
+            navc.view.transform = CGAffineTransformIdentity;
+            
+        } completion:^(BOOL finished) {
+
+            navc.view.layer.mask = nil;
+            
+        }];
+    }];
+    
     return YES;
 }
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- SplashAnimiationDemo/SplashAnimiationDemo/SplashAnimiationDemo/ViewController.m
+++ SplashAnimiationDemo/SplashAnimiationDemo/SplashAnimiationDemo/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.title = @"首页";
     // Do any additional setup after loading the view, typically from a nib.
 }
 
